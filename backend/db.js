1710caec80161b3888522c90e95f17d71f859269
Ivan Martynov <ivan.martynov@3tsoftwarelabs.com>
2024-12-04 14:24:41 +0100

    extracted dbName and collectionName

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,17 @@
 const { MongoClient } = require('mongodb');
 
 const uri = 'mongodb://localhost:27017'; // Replace with your MongoDB URI
+const dbName = 'test'; // Replace with your database name
+const collectionName = 'test'; // Replace with your collection name
+
 const client = new MongoClient(uri);
 
 async function executeQuery(query) {
   try {
     await client.connect();
     console.log("Connected")
-    const db = client.db('test'); // Replace with your database name
-    return await db.collection('test').find(query).toArray(); // Replace with your collection name
+    const db = client.db(dbName);
+    return await db.collection(collectionName).find(query).toArray();
   } catch (error) {
     console.error('Error executing query:', error);
     throw error;
