ccfd6de59f8f77baa88b3fc9d2733accd5deb458
Vladislav Lobanov <vlad.lobanov@3tsoftwarelabs.com>
2024-12-10 20:03:01 +0100

    Tests improved. Support for Tests on Windows added in the code

---
+++ tests/main-test.test.ts
@@ -0,0 +1,30 @@
+import { _electron as electron, ElectronApplication, Page } from "playwright";
+import assert from "assert/strict";
+import path from "path";
+import { describe, beforeAll, afterAll, test } from "@jest/globals";
+
+describe("Electron Application Tests", () => {
+  let electronApp: ElectronApplication;
+  let window: Page;
+
+  beforeAll(async () => {
+    // Launch Electron using ts-node to run main.ts
+    electronApp = await electron.launch({
+      args: [
+        // Use ts-node to run the TypeScript file directly
+        path.join(__dirname, "../dist-electron//main.js"), // Path to your Electron entry file
+      ],
+    });
+
+    window = await electronApp.firstWindow();
+  });
+
+  afterAll(async () => {
+    await electronApp.close();
+  });
+
+  test("Window title should be MongoDB Query Executor", async () => {
+    const title = await window.title();
+    assert.strictEqual(title, "MongoDB Query Executor");
+  });
+});
